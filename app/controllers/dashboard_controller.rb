42126e14a28a9ffc40eb25c418289e284ceed94b
leonardork <leodegolim@yahoo.com.br>
2018-04-22 18:16:33 -0300

    enviando array de valores da controller para a view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,17 @@
 class DashboardController < ApplicationController
 	def index
 
+		@arrayCores = [
+            "#abc123",
+            "#fff234"
+        ];
+
+        @valoresColuna = [
+            70,
+            130
+        ];
+
+
 		render "index"
 	end
 end
