5083ea5d95ec747642e9870548ddfa0a5ea47092
leonardork <leodegolim@yahoo.com.br>
2018-05-11 17:20:40 -0300

    atualizando gráfico a cada 1 segundo

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     resources :data_collects
   end
 
+  get 'temperature' => 'dashboards#get_temperature' , as: 'get_temperature'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
