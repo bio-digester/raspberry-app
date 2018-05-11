55445f0c2092bf26397c19c19506d943eabbefc9
leonardork <leodegolim@yahoo.com.br>
2018-05-11 17:21:15 -0300

    mostrando nome dos sensores em gráfico de pizza

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -9,6 +9,21 @@ class DashboardsController < ApplicationController
     @level = DataCollect.where(sensor_id: Sensor.find_by(name: "level").id)
     @water_flux = DataCollect.where(sensor_id: Sensor.find_by(name: "water_flux").id)
     # @ph = DataCollect.where(sensor_id: Sensor.find_by(name: "ph").id)
+    @dataCollectedFromSensorsWithName = getDataCollectedFromSensorsWithName
+  end
+
+  def getDataCollectedFromSensorsWithName
+    dataCollectedFromSensors = DataCollect.group(:sensor_id).count
+    dataCollectedFromSensorsWithName = Array.new
+    for sensor in dataCollectedFromSensors do
+      dataCollectedFromSensorsWithName.push([Sensor.find(sensor[0]).name, sensor[1]])
+    end
+    return dataCollectedFromSensorsWithName
+  end
+
+  def get_temperature
+    temperature = DataCollect.where(sensor_id: Sensor.find_by(name: "temperature").id)
+    render json: temperature.pluck(:data_measure, :value)
   end
 
 end
